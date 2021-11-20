ce642f43bef433d414f484f2509190b932ab231f
AngelFluffyOokami <dazzleookami@outlook.com>
2021-11-19 16:47:07 -0800

    Fix for the messy fix
    Finally figured things out surprisingly quickly this time.
    commandsinit is now imported w/o any errors! Hurray for basic programming skills I guess.

--- cinnamon.go
+++ cinnamon.go
@@ -2,31 +2,58 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/AngelFluffyOokami/Cinnamon/commandshandler/commandsschemas"
-
 	//	"gorm.io/gorm"
+	"github.com/AngelFluffyOokami/Cinnamon/commands"
 	"github.com/bwmarrin/discordgo"
 	//	"gorm.io/driver/sqlite3"
 )
 
+type Rankings struct {
+	Token string `json:"token"`
+}
 
 func main() {
 
+	schema := commands.MessageCreate
+
+	if _, err := os.Stat("./config.json"); err == nil {
+		log.Println("Configuration file found, loading configuration...")
+
+	} else if errors.Is(err, os.ErrNotExist) {
+		config := map[string]interface{}{
+			"token": "inserttokenhere",
+		}
+		data, err := json.MarshalIndent(config, "", "    ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		_ = ioutil.WriteFile("config.json", data, 0644)
+		log.Fatal("Configuration file not found! Creating new one. Please insert bot token into file before restarting")
+		return
+	} else {
+		log.Fatal(err)
+	}
 
 	jsonFile, err := jsonParse()
-	
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	token := jsonFile["Token"].(string)
+	token := jsonFile["token"].(string)
+	if token == "inserttokenhere" {
+		log.Fatal("No valid token has been detected, please insert bot token in the local configuration json file!")
+	}
+
 	dgo, err := discordgo.New("Bot " + token)
 
 	if err != nil {
@@ -34,10 +61,10 @@ func main() {
 		return
 	}
 
-	dgo.AddHandler(commandsschemas.MessageCreate)
+	dgo.AddHandler(schema)
 	dgo.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAll)
 	err = dgo.Open()
-	
+
 	if err != nil {
 		fmt.Println(err)
 		return
